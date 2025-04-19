f324936f547e961f40ac4fb37a202b8eac92522e
OmerS16 <omerseter@gmail.com>
2025-04-19 20:05:03 +0300

    Updated hameretz2

--- scrapers/hameretz2.py
+++ scrapers/hameretz2.py
@@ -4,27 +4,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
-from datetime import datetime
-import time
-import re
+import json
+from dateutil import parser
+import pytz
 
 def scrape():
-    heb_to_eng = {
-        "בינו׳": "Jan",
-        "בפבר׳":"Feb",
-        "במרץ":"Mar",
-        "באפר׳":"Apr",
-        "במאי":"May",
-        "ביונ׳":"Jun",
-        "ביול׳":"Jul",
-        "באוג׳":"Aug",
-        "בספט׳":"Sep",
-        "באוק׳":"Oct",
-        "בנוב׳":"Nov",
-        "בדצמ׳":"Dec"
-        }
-    
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
@@ -36,41 +20,32 @@ def scrape():
     # chrome_options.add_argument("--single-process")
     # chrome_options.add_argument("--remote-debugging-port=9222")
     # chrome_options.binary_location = "/usr/local/bin/chrome-linux64/chrome"
-    service = Service(r"C:\Users\Omer\Desktop\Coding\concerts project\chromedriver.exe")
+    service = Service(r"C:\Users\Omer\Desktop\Coding\concerts project\scraper\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
-    driver.get("https://www.hameretz2.org/")
+    driver.get("https://test.hameretz2.org/")
     
     wait = WebDriverWait(driver, 20)
-    events_section = wait.until(EC.presence_of_element_located((By.ID, "comp-m4r7k1ni")))
+    
+    events_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[data-category="לייב"]')))
+    urls = [e.get_attribute('href') for e in events_elements]
     
     events = []
     
-    for event in events_section.find_elements(By.CLASS_NAME, 'qElViY'):
-        title = event.find_element(By.CLASS_NAME, 'DjQEyU').get_attribute('textContent')
-        if any(word in title for word in ['קולנוע', 'לנדסקייפ']):
-            continue
-        wait = WebDriverWait(event, 20)
-        button = event.find_element(By.CSS_SELECTOR, ".sr_1jEj.sFpk6n3.sUMQ4T0.oDFuHIw---priority-4-link.s__6KuQ2f")
-        time.sleep(3)
-        button.click()
-        date_elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Ke8eTf.tnxjtm")))
-        date = date_elem.get_attribute('textContent').strip()
-        for heb, eng in heb_to_eng.items():
-            date = date.replace(heb, eng)
-        date, hour = date.split(', ', 1)
-        hour = hour[:5]
-        date = date + ' ' + str(hour)
-        date = datetime.strptime(date, '%d %b %Y %H:%M')
-        link = event.find_element(By.CSS_SELECTOR, '.DjQEyU.m022zm.aUkG34').get_attribute('href')
-        img = event.find_element(By.CSS_SELECTOR, '.L5u5gG.sGc29C2')
-        img = img.find_element(By.TAG_NAME, 'img').get_attribute('src')
-        img = img.replace('blur_2,', '')
-        img = re.sub(r"w_\d+", "w_500", img)
-        img = re.sub(r"h_\d+", "h_300", img)
-        events.append({'show_name':title, 'date':date, 'link':link, 'img':img, 'venue':'המרץ 2'})
+    for url in urls:
+        driver.get(url)
+        script = wait.until(EC.presence_of_element_located((By.ID, "__NEXT_DATA__"))).get_attribute("textContent")
+        data = json.loads(script)
+        event_details = data['props']['pageProps']['event']
+        show_name = event_details['title']
+        date = event_details['startDate']
+        tz = pytz.timezone("Asia/Jerusalem")
+        date = parser.isoparse(date).astimezone(tz)
+        date = date.strftime('%Y-%m-%d %H:%M:%S')
+        img = event_details['whatsappImage']
         
-    events = pd.DataFrame(events)
+        events.append({'show_name':show_name, 'date':date, 'link':url, 'img':img, 'venue':'המרץ2'})
+        
     return events
 
-events = hameretz2()
+events = scrape()
