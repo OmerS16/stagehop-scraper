ad1a0b41310b13d3a418d0c14cdcd3c2744e077f
OmerS16 <omerseter@gmail.com>
2025-05-02 22:43:55 +0300

    Updated Selenium Settings

--- scrapers/holybar.py
+++ scrapers/holybar.py
@@ -43,7 +43,8 @@ def scrape():
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--user-data-dir=/tmp/chrome-data")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.binary_location = "/usr/local/bin/chrome-linux64/chrome"
     service = Service('/usr/local/bin/chromedriver')
     driver = webdriver.Chrome(service=service, options=chrome_options)
