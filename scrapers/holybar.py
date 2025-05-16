334fbb6263a9f9d712639364ef6920e785d59f9e
OmerS16 <omerseter@gmail.com>
2025-05-16 18:15:41 +0300

    Updated Holy Bar Date Extraction

--- scrapers/holybar.py
+++ scrapers/holybar.py
@@ -7,14 +7,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import pandas as pd
 import re
-from datetime import datetime
+from datetime import datetime, date as Date
 import time
 import unicodedata
 from openai import OpenAI
 import os
 import json
+import calendar
 
 def scrape():
+    
+    FORMATS = [
+        "%A, %B %d",
+        "%A %B %d",
+        "%A %d.%m",
+        "%A"
+        ]
+    
+    POST_PATHS = [
+        "../../following-sibling::*[h1]/h1",
+        "../following-sibling::span"
+        ]
+    
     def find_matches(driver, pattern):
         match = False
         wait = WebDriverWait(driver, 5)
@@ -48,56 +62,79 @@ def scrape():
         service = Service('/usr/local/bin/chromedriver')
         driver = webdriver.Chrome(service=service, options=chrome_options)
         
-        driver.get('https://www.instagram.com/holybar/p/DF2QTMlIsGL/?img_index=1')
+        random_post = 'https://www.instagram.com/holybar/p/DF2QTMlIsGL/?img_index=1'
+        driver.get(random_post)
         
         time.sleep(10)
         
         pattern = re.compile(r'(sessions?|lineup)', re.IGNORECASE)
         
         links = driver.find_elements(By.TAG_NAME, 'a')
-        links_url = [link.get_attribute('href') for link in links]
+        links_url = [[link.get_attribute('href'), link.find_element(By.TAG_NAME, 'img').get_attribute('src')] for link in links if link.find_elements(By.TAG_NAME, 'img')]
         
-        post_pattern = re.compile(r'https://www.instagram.com/holybar/p/')
-        posts = [link_url for link_url in links_url if post_pattern.search(link_url or '')]
+        post_pattern = re.compile(r'https://www.instagram.com/holybar/(?:p|reel)/')
+        posts = [(href,src) for href, src in links_url if post_pattern.search(href or '')]
         
         last_post = posts[1]
-        driver.get(last_post)
-        time.sleep(10)
-        
+        last_post_img = last_post[1]
+        driver.get(last_post[0])
+        time.sleep(15)
+    
+     
         profile_links = driver.find_elements(By.CSS_SELECTOR, "a[href='/holybar/']")
-        matches = []
+        
+        match = None
+
         for elem in profile_links:
-            try:
-                post_body = elem.find_element(By.XPATH, "../../following-sibling::*[h1]/h1")
-                print("post body found")
-                post_text = post_body.text
-                post_text_clean = unicodedata.normalize("NFKC", post_text)
-                post_text_clean = re.sub(r'[^\x00-\x7F]+', '', post_text_clean)
-                post_text_clean = re.sub(r'^\s+', '', post_text_clean, flags=re.MULTILINE)
-                lineup = pattern.search(post_text_clean)
-                if lineup:
-                    print('lineup word found, validating results..')
+            for path in POST_PATHS:
+                try:
+                    post_body = elem.find_element(By.XPATH, path)
+                    print("post body found")
+                    post_text = post_body.text
+                    post_text_clean = unicodedata.normalize("NFKC", post_text)
+                    post_text_clean = re.sub(r'[^\x00-\x7F]+', '', post_text_clean)
+                    post_text_clean = re.sub(r'^\s+', '', post_text_clean, flags=re.MULTILINE)
                     day_pattern = r'^(?:Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday).*'
-                    matches = re.findall(day_pattern, post_text_clean, flags=re.MULTILINE)
+                    match = re.findall(day_pattern, post_text_clean, flags=re.MULTILINE)
+                except:
+                    print('not found, continuing..')
+                    continue
+                if match:
                     break
-            except:
-                print('not found, continuing..')
-                continue
+            if match:
+                break
         
         events = []
+        matching_img = None
         
-        if matches:
-            for line in matches:
-                try:
-                    date, title = line.split(' - ', 1)
-                except:
-                    date, title = line.split('  ', 1)
-                title = title.strip()
-                date = date + ' 20:45'
-                date = datetime.strptime(date, '%A, %B %d %H:%M')
-                date = date.replace(year=datetime.now().year)
-                events.append({'show_name':title, 'date':date})
+        post_date = driver.find_element(By.TAG_NAME, 'time').get_attribute('datetime')
+        post_date = datetime.strptime(post_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+                
         
+        if match:
+            for line in match:
+                for symbol in [' - ', '  ']:
+                    try:
+                        date, title = line.split(symbol, 1)
+                        title = title.strip()
+                        for frmt in FORMATS:
+                            try:
+                                if frmt == "%A":
+                                    day = list(calendar.day_name).index(date) + 1
+                                    year, week, _ = post_date.isocalendar()
+                                    if day != 7:
+                                        week += 1
+                                    date = Date.fromisocalendar(year, week, day)
+                                else:
+                                    date = datetime.strptime(date, frmt)
+                            except ValueError:
+                                continue    
+                                
+                        date = date.replace(year=datetime.now().year)
+                        events.append({'show_name':title, 'date':date})
+                    except:
+                        continue
+
         else:
             print("No lineup found in post's body..\nActivating AI..")
             matching_img = find_matches(driver, pattern)
@@ -112,7 +149,7 @@ def scrape():
                     input=[{
                         "role": "user",
                         "content": [
-                            {"type": "input_text", "text": "Extract all the shows' names and dates to json with keys: show_name, date. Do not add any text to the response so I could convert your response to json."},
+                            {"type": "input_text", "text": "Extract all the shows' names and dates to json with keys: show_name, date. The default hour is 20:45, return date in format %Y-%m-%d %H:%M. Do not add any text to the response so I could convert your response to json."},
                             {
                                 "type": "input_image",
                                 "image_url": matching_img,
@@ -135,10 +172,10 @@ def scrape():
                 print('Bad JSON given by AI')
             
         events = pd.DataFrame(events)
-        events['date'] = events['date'].apply(lambda x: datetime.strptime((x + ' 20:45'), '%d.%m %H:%M'))
+        events['date'] = pd.to_datetime(events['date'], format='%Y-%m-%d %H:%M', errors='coerce')
         events['date'] = events['date'].apply(lambda x: x.replace(year=datetime.now().year))
-        events['link'] = last_post
-        events['img'] = matching_img
+        events['link'] = last_post[0]
+        events['img'] = matching_img or last_post_img
         events['venue'] = 'Holy Bar'
 
     finally:
