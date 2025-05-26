b11ce735a8857e0f54b0e935da3934988f9b1d58
OmerS16 <omerseter@gmail.com>
2025-05-26 23:20:36 +0300

    Updated Code

--- scrapers/holybar.py
+++ scrapers/holybar.py
@@ -31,12 +31,13 @@ def scrape():
     
     def find_matches(driver, pattern):
         match = False
-        wait = WebDriverWait(driver, 5)
         
         while True:
-            images = driver.find_elements(By.TAG_NAME, "img")
-            for img in images:
+            filtered_imgs = driver.find_elements(By.XPATH, "(//div[@class='_ap3a _aaco _aacw _aacy _aad6'])[1]/preceding::img")
+            for img in filtered_imgs:
+                print(img.get_attribute('class'))
                 alt = img.get_attribute('alt') or ''
+                print(alt)
                 if pattern.search(alt):
                     match = img.get_attribute('src')
                     return match
@@ -48,26 +49,27 @@ def scrape():
             except (NoSuchElementException, TimeoutException):
                 break
             
-            return match
+        return match
     try:
         user_data_dir = tempfile.mkdtemp(prefix="chrome-data-")
         chrome_options = Options()
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--window-size=1920,1080")
+        # chrome_options.add_argument("--headless=new")
+        # chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-setuid-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--single-process")
-        chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
-        chrome_options.add_argument("--no-first-run")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-background-networking")
-        chrome_options.add_argument("--disable-default-apps")
-        chrome_options.add_argument("--disable-features=VizDisplayCompositor")
-        chrome_options.binary_location = "/usr/local/bin/chrome-linux64/chrome"
-        service = Service('/usr/local/bin/chromedriver')
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--disable-setuid-sandbox")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--single-process")
+        # chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+        # chrome_options.add_argument("--no-first-run")
+        # chrome_options.add_argument("--disable-extensions")
+        # chrome_options.add_argument("--disable-background-networking")
+        # chrome_options.add_argument("--disable-default-apps")
+        # chrome_options.add_argument("--disable-features=VizDisplayCompositor")
+        # chrome_options.binary_location = "/usr/local/bin/chrome-linux64/chrome"
+        # service = Service('/usr/local/bin/chromedriver')
+        service = Service(r"C:\Users\Omer\Desktop\Coding\concerts project\scraper\chromedriver.exe")
         try:
             print("Launching Chrome...")
             driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -80,6 +82,10 @@ def scrape():
         
         time.sleep(10)
         
+        wait = WebDriverWait(driver, 5)
+        button_overlay = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "svg[aria-label='Close']")))
+        button_overlay.click()
+        
         pattern = re.compile(r'(sessions?|lineup)', re.IGNORECASE)
         
         links = driver.find_elements(By.TAG_NAME, 'a')
@@ -88,11 +94,10 @@ def scrape():
         post_pattern = re.compile(r'https://www.instagram.com/holybar/(?:p|reel)/')
         posts = [(href,src) for href, src in links_url if post_pattern.search(href or '')]
         
-        last_post = posts[1]
-        last_post_img = last_post[1]
+        last_post = posts[0]
+        last_post_img = last_post[0]
         driver.get(last_post[0])
         time.sleep(15)
-    
      
         profile_links = driver.find_elements(By.CSS_SELECTOR, "a[href='/holybar/']")
         
